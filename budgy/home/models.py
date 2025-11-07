40fff5fac184dd1719e9ebf8530471e5ce5bb65d
Natthasit Thitithammakun <6610685155@student.tu.ac.th>
2025-11-07 22:55:27 +0700

    add page

--- budgy/home/models.py
+++ budgy/home/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+# Category Model
+class Category(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category_name = models.CharField(max_length=100, primary_key=True)
+    trans_type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.category_name}"
+
+
+# Month Report Model
+class MonthReport(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    report_id = models.AutoField(primary_key=True)
+    month = models.IntegerField()
+    year = models.IntegerField()
+    income_total = models.FloatField()
+    expense_total = models.FloatField()
+
+    def __str__(self):
+        return f"Report for {self.month}/{self.year}"
+
+
+# Account Model
+class Account(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    account_name = models.CharField(max_length=100, primary_key=True)
+    type_acc = models.CharField(max_length=50)
+    balance = models.FloatField()
+
+    def __str__(self):
+        return self.account_name
+
+
+# Transaction Model
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    trans_id = models.AutoField(primary_key=True)
+    trans_type = models.CharField(max_length=50)
+    date = models.DateTimeField()
+    amount = models.FloatField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Date: {self.date.day}/{self.date.month}/{self.date.year} amount:{self.amount}"
+
+
+# Transaction Income model
+class Income(Transaction):
+    to_account = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+
+# Transaction Expense model
+class Expense(Transaction):
+    from_account = models.ForeignKey(Account, on_delete=models.CASCADE)
--- budgy/home/views.py
+++ budgy/home/views.py
@@ -1,10 +1,45 @@
-from django.shortcuts import render
-from django.template import loader
-
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.db.models import Sum
+from datetime import datetime
+from django.urls import reverse
+from .models import Transaction, Account, Category, MonthReport, Income, Expense
 
 
 # Create your views here.
-def home(request):
-    template = loader.get_template("home.html")
-    return HttpResponse(template.render())
+def home_page(request, username):
+    user = get_object_or_404(User, username=username)
+
+    return render(request, "home/home.html", {"user": user})
+
+
+def dashboard_today_page(request):
+
+    return render(request, "home/dashboard.html")
+
+
+def back_month_dashboard(request):
+
+    return render(request, "home/dashboard.html")
+
+
+def transaction_income_page(request):
+
+    return render(request, "home/transaction.html", context)
+
+
+def transaction_expense_page(request):
+    return render(request, "home/transaction.html")
+
+
+def transaction_transfer_page(request):
+    return render(request, "home/transaction.html")
+
+
+def stats_page(request):
+    return render(request, "home/stats.html")
+
+
+def settings_page(request):
+    return render(request, "home/settings.html")
