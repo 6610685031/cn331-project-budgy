ccf9d7cdafcc3572ff96de063f9d275c3257e658
Krentiz <48907723+Krentiz@users.noreply.github.com>
2025-11-07 23:33:52 +0700

    add urls for home app

--- budgy/home/urls.py
+++ budgy/home/urls.py
@@ -4,4 +4,20 @@ from . import views
 urlpatterns = [
     path("", views.home_page, name="home"),
     path("home/", views.home_page, name="home"),
+    path("dashboard/", views.dashboard_today_page, name="dashboard"),
+    path(
+        "transaction/income/", views.transaction_income_page, name="transaction_income"
+    ),
+    path(
+        "transaction/expense/",
+        views.transaction_expense_page,
+        name="transaction_expense",
+    ),
+    path(
+        "transaction/transfer/",
+        views.transaction_transfer_page,
+        name="transaction_transfer",
+    ),
+    path("stats/", views.stats_page, name="stats"),
+    path("settings/", views.settings_page, name="settings"),
 ]
