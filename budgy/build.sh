339acd55ccce13f4683618397b4eca3e0ebb8797
Krentiz <48907723+Krentiz@users.noreply.github.com>
2025-11-09 21:03:48 +0700

    update build.sh

--- budgy/build.sh
+++ budgy/build.sh
@@ -8,3 +8,4 @@ pip install -r requirements.txt
 python manage.py flush --no-input
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py createsuperuser --no-input --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
